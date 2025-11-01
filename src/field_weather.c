502002e748dd391c7f43f7a5502806dba8a4e3fb
A. Wheeler <wheelercs@protonmail.com>
2025-11-01 18:52:35 -0500

    Reimplemented Snow Weather
    Reimplemented the snow overworld weather and tied in-battle hail to it.
    
    Signed-off-by: A. Wheeler <wheelercs@protonmail.com>

--- src/field_weather.c
+++ src/field_weather.c
@@ -4,6 +4,7 @@
 #include "constants/rgb.h"
 #include "util.h"
 #include "event_object_movement.h"
+#include "field_camera.h"
 #include "field_weather.h"
 #include "main.h"
 #include "menu.h"
@@ -266,6 +267,7 @@ static void Task_WeatherInit(u8 taskId)
     // When the screen fades in, this is set to TRUE.
     if (gWeatherPtr->readyForInit)
     {
+        UpdateCameraPanning();
         sWeatherFuncs[gWeatherPtr->currWeather].initAll();
         gTasks[taskId].func = Task_WeatherMain;
     }
